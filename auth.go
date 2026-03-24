255452ff67733bd81174d5b94c33788f35ea42a7
SkyeAv <skye.lane.goetz@gmail.com>
2026-03-24 14:49:43 -0700

    Implemented Redis Auth

--- auth.go
+++ auth.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
 
-func auth() {
-	fmt.Println("Hi")
+	"github.com/cespare/xxhash/v2"
+	"github.com/redis/go-redis/v9"
+)
+
+func getHashHex(apiKey string) string {
+	data := []byte(apiKey)
+	asInt := xxhash.Sum64(data)
+
+	return fmt.Sprintf("%x", asInt)
+}
+
+var ctx = context.Background()
+
+func auth(username string, apiKey string) bool {
+	opts := &redis.Options{Addr: "localhost:6379", Password: "", DB: 0}
+	rdb := redis.NewClient(opts)
+	defer rdb.Close()
+
+	hash, _ := rdb.Get(ctx, username).Result()
+	if hash == getHashHex(apiKey) {
+		return true
+	}
+
+	return false
 }
--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func health(c *gin.Context) {
+	c.JSON(200, gin.H{"ok": 200})
+}
+
+func registerRoutes(r *gin.Engine) {
+	r.GET("/health/", health)
+}
 
 func main() {
-	fmt.Println("Hi")
+	r := gin.Default()
+	registerRoutes(r)
+	r.Run(":8550")
 }
