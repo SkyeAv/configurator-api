33e6c62bf7cc56e2156771d55d3b72eeb308126a
SkyeAv <skye.lane.goetz@gmail.com>
2026-04-13 12:29:27 -0700

    Added New Tablassist Script

---
+++ tablassist.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+var macOSAppleSiliconZshInstallScript string = os.Getenv("MAC_OS_APPLE_SILICON_INSTALL_SCRIPT")
+
+func MacOSAppleSiliconZshInstaller(c *gin.Context) {
+	file, err := os.Open(macOSAppleSiliconZshInstallScript)
+	if err != nil {
+		c.JSON(404, gin.H{"error": err.Error()})
+		return
+	}
+	defer file.Close()
+
+	c.Header("Content-Type", "application/octet-stream")
+	io.Copy(c.Writer, file)
+}
